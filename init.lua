378491df2c21880c6fbe099327dfbd5900210e85
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-24 22:32:55 -0600

    refactor: Update API calls to use 'vim' module

--- init.lua
+++ init.lua
@@ -3,28 +3,26 @@
 
 local typewriter = {}
 
-local api = vim.api
-
 local typewriter_active = false
 
 local function center_cursor()
 	if not typewriter_active then
 		return
 	end
-	local win_height = api.nvim_win_get_height(0)
-	local cursor = api.nvim_win_get_cursor(0)
+	local win_height = vim.api.nvim_win_get_height(0)
+	local cursor = vim.api.nvim_win_get_cursor(0)
 
 	local new_row = math.floor(win_height / 2)
 	local new_col = cursor[2]
 
-	api.nvim_win_set_cursor(0, { new_row, new_col })
-	api.nvim_command("normal! zz")
+	vim.api.nvim_win_set_cursor(0, { new_row, new_col })
+	vim.api.nvim_command("normal! zz")
 end
 
 local function enable_typewriter_mode()
 	if not typewriter_active then
 		typewriter_active = true
-		api.nvim_command('autocmd CursorMoved * lua require("typewriter").center_cursor()')
+		vim.api.nvim_command('autocmd CursorMoved * lua require("typewriter").center_cursor()')
 		print("Typewriter mode enabled")
 	end
 end
@@ -32,7 +30,7 @@ end
 local function disable_typewriter_mode()
 	if typewriter_active then
 		typewriter_active = false
-		api.nvim_command("autocmd! TypewriterMode")
+		vim.api.nvim_command("autocmd! TypewriterMode")
 		print("Typewriter mode disabled")
 	end
 end
@@ -46,13 +44,13 @@ local function toggle_typewriter_mode()
 end
 
 function typewriter.setup()
-	api.nvim_command("augroup TypewriterMode")
-	api.nvim_command("autocmd!")
-	api.nvim_command("augroup END")
+	vim.api.nvim_command("augroup TypewriterMode")
+	vim.api.nvim_command("autocmd!")
+	vim.api.nvim_command("augroup END")
 
-	api.nvim_create_user_command("EnableTypewriter", enable_typewriter_mode, {})
-	api.nvim_create_user_command("DisableTypewriter", disable_typewriter_mode, {})
-	api.nvim_create_user_command("ToggleTypewriter", toggle_typewriter_mode, {})
+	vim.api.nvim_create_user_command("EnableTypewriter", enable_typewriter_mode, {})
+	vim.api.nvim_create_user_command("DisableTypewriter", disable_typewriter_mode, {})
+	vim.api.nvim_create_user_command("ToggleTypewriter", toggle_typewriter_mode, {})
 end
 
 return typewriter
