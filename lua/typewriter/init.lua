7e59ea49a36390cd2e26596b3b1bf4d8877f2db6
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-09 01:02:01 -0600

    feat(init): Added functionality to enable with True Zen and ZenMode plugins

--- lua/typewriter/init.lua
+++ lua/typewriter/init.lua
@@ -7,6 +7,11 @@ local api = vim.api
 
 local typewriter_active = false
 
+local config = {
+	enable_with_zen_mode = true,
+	enable_with_true_zen = true,
+}
+
 local function center_cursor()
 	if not typewriter_active then
 		return
@@ -25,6 +30,7 @@ end
 local function enable_typewriter_mode()
 	if not typewriter_active then
 		typewriter_active = true
+		-- Set autocommands for normal, insert, and visual modes
 		api.nvim_command('autocmd CursorMoved,CursorMovedI * lua require("typewriter").center_cursor()')
 		print("Typewriter mode enabled")
 	end
@@ -33,6 +39,7 @@ end
 local function disable_typewriter_mode()
 	if typewriter_active then
 		typewriter_active = false
+		-- Clear the autocommand group for TypewriterMode
 		api.nvim_command("autocmd! TypewriterMode")
 		print("Typewriter mode disabled")
 	end
@@ -46,7 +53,11 @@ local function toggle_typewriter_mode()
 	end
 end
 
-local function setup()
+local function setup(user_config)
+	if user_config then
+		config = vim.tbl_extend("force", config, user_config)
+	end
+
 	api.nvim_command("augroup TypewriterMode")
 	api.nvim_command("autocmd!")
 	api.nvim_command("augroup END")
@@ -54,6 +65,16 @@ local function setup()
 	api.nvim_create_user_command("TWEnable", enable_typewriter_mode, {})
 	api.nvim_create_user_command("TWDisable", disable_typewriter_mode, {})
 	api.nvim_create_user_command("TWToggle", toggle_typewriter_mode, {})
+
+	if config.enable_with_zen_mode then
+		api.nvim_command('autocmd User ZenModePre lua require("typewriter").enable()')
+		api.nvim_command('autocmd User ZenModeLeave lua require("typewriter").disable()')
+	end
+
+	if config.enable_with_true_zen then
+		api.nvim_command('autocmd User TZWoon lua require("typewriter").enable()')
+		api.nvim_command('autocmd User TZOff lua require("typewriter").disable()')
+	end
 end
 
 return {
