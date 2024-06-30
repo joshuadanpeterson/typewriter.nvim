3771c2bf0a90989b6494ef5d96003197c88e553c
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-29 23:18:31 -0600

    feat(typewriter): Add typewriter scrolling functionality
    Add cursor relative to text when using TWTop and TWBottom commands

--- lua/typewriter/init.lua
+++ lua/typewriter/init.lua
@@ -2,12 +2,18 @@
 -- This plugin provides typewriter scrolling for neovim.
 
 local api = vim.api
-local typewriter_active = false
-local config = {
+local ts_utils = require("nvim-treesitter.ts_utils")
+local center_block_config = require("typewriter.utils.center_block_config")
+
+local M = {}
+
+M.config = {
 	enable_with_zen_mode = true,
 	enable_with_true_zen = true,
 	keep_cursor_position = true,
 }
+
+local typewriter_active = false
 
 local function center_cursor()
 	if not typewriter_active then
@@ -44,12 +50,9 @@ local function toggle_typewriter_mode()
 end
 
 local function center_block_and_cursor()
-	local ts_utils = require("nvim-treesitter.ts_utils")
-	local config = require("typewriter.utils.center_block_config")
-
 	local function is_significant_block(node)
 		local node_type = node:type()
-		return config.expand[node_type] ~= nil
+		return center_block_config.expand[node_type] == true
 	end
 
 	local function get_expand_root(node)
@@ -80,16 +83,19 @@ local function center_block_and_cursor()
 	local middle_line = math.floor((start_row + end_row) / 2)
 
 	-- Center the view around the middle line without moving the cursor
-	vim.cmd("normal! zz")
+	if M.config.keep_cursor_position then
+		local cursor = api.nvim_win_get_cursor(0)
+		vim.cmd("normal! zz")
+		api.nvim_win_set_cursor(0, cursor)
+	else
+		vim.cmd("normal! zz")
+	end
 end
 
 local function move_to_top_of_block()
-	local ts_utils = require("nvim-treesitter.ts_utils")
-	local config = require("typewriter.utils.center_block_config")
-
 	local function is_significant_block(node)
 		local node_type = node:type()
-		return config.expand[node_type] ~= nil
+		return center_block_config.expand[node_type] == true
 	end
 
 	local function get_expand_root(node)
@@ -116,23 +122,31 @@ local function move_to_top_of_block()
 	-- Get the range of the block/function
 	local start_row, _, _, _ = node:range()
 
+	local cursor_row, cursor_col
+	if M.config.keep_cursor_position then
+		-- Save the current cursor position relative to the block
+		local cursor = api.nvim_win_get_cursor(0)
+		cursor_row = cursor[1] - start_row
+		cursor_col = cursor[2]
+	end
+
 	-- Move the cursor to the start of the block
 	vim.api.nvim_win_set_cursor(0, { start_row + 1, 0 })
 	-- Scroll so that the top of the block is at the top of the window
 	vim.cmd("normal! zt")
-	-- Add offset to ensure the top line is visible
-	vim.schedule(function()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("1<C-y>", true, true, true), "n", true)
-	end)
+
+	if M.config.keep_cursor_position then
+		-- Restore the cursor position relative to the text
+		vim.schedule(function()
+			vim.api.nvim_win_set_cursor(0, { start_row + 1 + cursor_row, cursor_col })
+		end)
+	end
 end
 
 local function move_to_bottom_of_block()
-	local ts_utils = require("nvim-treesitter.ts_utils")
-	local config = require("typewriter.utils.center_block_config")
-
 	local function is_significant_block(node)
 		local node_type = node:type()
-		return config.expand[node_type] ~= nil
+		return center_block_config.expand[node_type] == true
 	end
 
 	local function get_expand_root(node)
@@ -157,22 +171,33 @@ local function move_to_bottom_of_block()
 	end
 
 	-- Get the range of the block/function
-	local _, _, end_row, _ = node:range()
+	local start_row, _, end_row, _ = node:range()
+
+	local cursor_row, cursor_col
+	if M.config.keep_cursor_position then
+		-- Save the current cursor position relative to the block
+		local cursor = api.nvim_win_get_cursor(0)
+		cursor_row = cursor[1] - start_row
+		cursor_col = cursor[2]
+	end
 
 	-- Move the cursor to the end of the block
 	vim.api.nvim_win_set_cursor(0, { end_row + 1, 0 })
 	-- Scroll so that the bottom of the block is at the bottom of the window
 	vim.cmd("normal! zb")
-	-- Add offset to ensure the bottom line is visible
-	vim.schedule(function()
-		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("1<C-e>", true, true, true), "n", true)
-	end)
+
+	if M.config.keep_cursor_position then
+		-- Restore the cursor position relative to the text
+		vim.schedule(function()
+			vim.api.nvim_win_set_cursor(0, { start_row + 1 + cursor_row, cursor_col })
+		end)
+	end
 end
 
 local function setup(user_config)
 	user_config = user_config or {}
 	if type(user_config) == "table" then
-		config = vim.tbl_extend("force", config, user_config)
+		M.config = vim.tbl_extend("force", M.config, user_config)
 	end
 	api.nvim_command("augroup TypewriterMode")
 	api.nvim_command("autocmd!")
@@ -183,23 +208,23 @@ local function setup(user_config)
 	api.nvim_create_user_command("TWCenter", center_block_and_cursor, {})
 	api.nvim_create_user_command("TWTop", move_to_top_of_block, {})
 	api.nvim_create_user_command("TWBottom", move_to_bottom_of_block, {})
-	if config.enable_with_zen_mode then
+	if M.config.enable_with_zen_mode then
 		api.nvim_command('autocmd User ZenModePre lua require("typewriter").enable()')
 		api.nvim_command('autocmd User ZenModeLeave lua require("typewriter").disable()')
 	end
-	if config.enable_with_true_zen then
+	if M.config.enable_with_true_zen then
 		api.nvim_command('autocmd User TZWoon lua require("typewriter").enable()')
 		api.nvim_command('autocmd User TZOff lua require("typewriter").disable()')
 	end
 end
 
-return {
-	setup = setup,
-	center_cursor = center_cursor,
-	enable = enable_typewriter_mode,
-	disable = disable_typewriter_mode,
-	toggle = toggle_typewriter_mode,
-	center_block_and_cursor = center_block_and_cursor,
-	move_to_top_of_block = move_to_top_of_block,
-	move_to_bottom_of_block = move_to_bottom_of_block,
-}
+M.setup = setup
+M.center_cursor = center_cursor
+M.enable = enable_typewriter_mode
+M.disable = disable_typewriter_mode
+M.toggle = toggle_typewriter_mode
+M.center_block_and_cursor = center_block_and_cursor
+M.move_to_top_of_block = move_to_top_of_block
+M.move_to_bottom_of_block = move_to_bottom_of_block
+
+return M
